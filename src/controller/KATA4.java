fcc28cae06beef411527aa3cb3c393c6a2db5669
MarcosJSP <marcos.academia.2@gmail.com>
2018-10-26 10:20:57 +0200

    V 2.0

--- src/controller/KATA4.java
+++ src/controller/KATA4.java
@@ -10,16 +10,41 @@ import view.MailHistogramBuilder;
 import view.MailListReader;
 
 public class KATA4 {
-    public static void main(String[] args) throws IOException, ClassNotFoundException{
-        
-        String fileName = "email.txt";
-        MailListReader reader = new MailListReader();
-        List<Mail> mailList = reader.read(fileName);
-        
-        MailHistogramBuilder builder = new MailHistogramBuilder();
-        Histogram<String> histogram = builder.build(mailList);
-        
-        HistogramDisplay histoDisplay = new HistogramDisplay(histogram);
+    private String fileName;
+    private MailListReader reader;
+    private List<Mail> mailList;
+    private MailHistogramBuilder builder;
+    private Histogram<String> histogram;
+    private HistogramDisplay histoDisplay;
+    
+    public static void main(String[] args){
+        KATA4 m = new KATA4();
+        m.control();
+    }
+    
+    public void control(){
+        input();
+        process();
+        output();
+    }
+    
+    public void input(){
+        try {
+            fileName = "email.txt";
+            reader = new MailListReader();
+            mailList = reader.read(fileName);
+        } catch (Exception e) {
+            System.err.println("Hubo un error con la lectura del archivo");
+        }
+    }
+    
+    public void process(){
+        builder = new MailHistogramBuilder();
+        histogram = builder.build(mailList);
+    }
+    
+    public void output(){
+        histoDisplay = new HistogramDisplay(histogram);
         histoDisplay.execute();
     }
     
